f233d7d820cbdbdd2e8d2e013e916fe0571ce73a
Anderson Rossanez <ra124136@students.ic.unicamp.br>
2016-08-14 12:44:34 -0300

    Adding workaround for catching nested NUMBER_AND_UNIT rules.

--- autochecklist/src/com/autochecklist/utils/nlp/ExpressionExtractor.java
+++ autochecklist/src/com/autochecklist/utils/nlp/ExpressionExtractor.java
@@ -31,6 +31,22 @@ public class ExpressionExtractor {
 				String matchType = formatMatchValue(match.getValue().toString());
 				String value = match.toString();
 				retList.add(new Pair<String, String>(matchType, value));
+
+				// This is a workaround for dealing with nested tokens.
+				if (hasPotentialNumbersAndUnits(matchType)) {
+					int initPos = text.indexOf(value);
+					String newText = text.substring(initPos);
+					
+					int initNumberPos = value.lastIndexOf(' ');
+					newText = newText.substring(initNumberPos).trim();
+					
+					List<Pair<String, String>> innerRetList = extract(newText);
+					if (!innerRetList.isEmpty()) {
+						retList.addAll(innerRetList);
+					}
+					
+					return retList;
+				}
 			}
 		}
 
@@ -41,5 +57,16 @@ public class ExpressionExtractor {
 		int init = value.lastIndexOf('(');
 		int end = value.lastIndexOf(')');
 		return value.substring(init + 1, end);
+	}
+
+	private boolean hasPotentialNumbersAndUnits(String matchType) {
+		if ("IN_MORE_THAN".equals(matchType)
+			|| "IN_LESS_THAN".equals(matchType)
+			|| "IN_SOME".equals(matchType)
+			|| "A_FEW".equals(matchType)) {
+			return true;
+		}
+
+		return false;
 	}
 }
