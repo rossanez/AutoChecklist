ac1065eb49bcc847dfdb6fd73efe92c8543a3182
Anderson Rossanez <ra124136@students.ic.unicamp.br>
2016-09-18 22:06:42 -0300

    Further development of the actions-events detector (not ready).

--- autochecklist/src/com/autochecklist/utils/nlp/EventActionDetector.java
+++ autochecklist/src/com/autochecklist/utils/nlp/EventActionDetector.java
@@ -1,17 +1,34 @@
 package com.autochecklist.utils.nlp;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.autochecklist.utils.Utils;
+
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+import edu.stanford.nlp.trees.tregex.TregexPattern;
 import edu.stanford.nlp.util.CoreMap;
 
 public class EventActionDetector {
 
 	private LexicalizedParser mParser;
+	private Set<String> mActionEventIndicators;
 
 	public EventActionDetector() {
 		mParser = CoreNLP.getInstance().getParser();
+		initActionEventIndicators();
+	}
+
+	private void initActionEventIndicators() {
+		mActionEventIndicators = new HashSet<String>();
+		mActionEventIndicators.add("if");
+		mActionEventIndicators.add("else");
 	}
 
 	public boolean detect(String text) {
@@ -25,8 +42,31 @@ public class EventActionDetector {
 	}
 
 	private boolean detectOnSentece(String sentence) {
-		// TODO implement it!
-		Tree parseTree = null;//mParser.parse(sentence);
-		return parseTree != null;
+		Tree parseTree = mParser.parse(sentence);
+
+		TregexMatcher sentenceMatcher = TregexPattern.compile("ROOT << SBAR=sbar").matcher(parseTree);
+
+        while (sentenceMatcher.find()) {
+        	Tree sbarPhraseTree = sentenceMatcher.getNode("sbar");
+
+            TregexMatcher sbarPhraseMatcher = TregexPattern.compile("SBAR <<, IN=in").matcher(sbarPhraseTree);
+
+            while (sbarPhraseMatcher.find()) {
+            	List<Word> words = sbarPhraseMatcher.getNode("in").yieldWords();
+            	if ((words.size() != 1) || !isActionEventIndicator(words.get(0).toString())) {
+            		break;
+            	}
+
+            	return true;
+            }
+        }
+
+		return false;
+	}
+
+	private boolean isActionEventIndicator(String word) {
+		if (Utils.isTextEmpty(word)) return false;
+
+		return mActionEventIndicators.contains(word.toLowerCase());
 	}
 }
