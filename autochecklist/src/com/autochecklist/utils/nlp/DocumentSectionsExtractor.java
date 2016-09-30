33c2c26a03d9b05044bc0b6073978ceb145db3a7
Anderson Rossanez <ra124136@students.ic.unicamp.br>
2016-09-30 15:41:37 -0300

    Really smallchange on DocumentSectionsExtractor class.

--- autochecklist/src/com/autochecklist/utils/nlp/DocumentSectionsExtractor.java
+++ autochecklist/src/com/autochecklist/utils/nlp/DocumentSectionsExtractor.java
@@ -10,12 +10,16 @@ public class DocumentSectionsExtractor {
 
 	public void extract(String document) {
 		BufferedReader bufReader = new BufferedReader(new StringReader(document));
-		String line=null;
+		String line = null;
 		try {
-			while( (line=bufReader.readLine()) != null ) {
-			    if (!Utils.isTextEmpty(line) && Character.isDigit(line.charAt(0))) {
-				    //Utils.println(line);
-			    }
+			while ((line = bufReader.readLine()) != null) {
+				if (!Utils.isTextEmpty(line)) {
+					String[] title = line.split(" ");
+					if ((title == null) || (title.length < 2)) continue;
+					if (!title[0].matches("[0-9]+(\\.[0-9]+)*")) continue;
+
+					Utils.println(line);
+				}
 			}
 		} catch (IOException e) {
 			Utils.printError("Error when trying to extract the document sections!");
