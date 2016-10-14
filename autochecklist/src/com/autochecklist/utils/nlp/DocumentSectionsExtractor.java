5670a40d15682491a996d70fef7a826490da76c4
Anderson Rossanez <ra124136@students.ic.unicamp.br>
2016-10-14 11:19:00 -0300

    Improving the document sections extractor.

--- autochecklist/src/com/autochecklist/utils/nlp/DocumentSectionsExtractor.java
+++ autochecklist/src/com/autochecklist/utils/nlp/DocumentSectionsExtractor.java
@@ -3,26 +3,42 @@ package com.autochecklist.utils.nlp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import com.autochecklist.utils.Pair;
 import com.autochecklist.utils.Utils;
 
 public class DocumentSectionsExtractor {
 
-	public void extract(String document) {
+	private ExpressionExtractor mSectionIdExtractor;
+
+	public DocumentSectionsExtractor() {
+		this.mSectionIdExtractor = new ExpressionExtractor("RegexRules/sectionid.rules");
+	}
+	
+	public Set<String> extract(String document) {
+		Set<String> sections = new HashSet<String>();
+
 		BufferedReader bufReader = new BufferedReader(new StringReader(document));
 		String line = null;
 		try {
 			while ((line = bufReader.readLine()) != null) {
 				if (!Utils.isTextEmpty(line)) {
-					String[] title = line.split(" ");
-					if ((title == null) || (title.length < 2)) continue;
-					if (!title[0].matches("[0-9]+(\\.[0-9]+)*")) continue;
+					String[] titleCandidate = line.split(" ");
+					if ((titleCandidate == null) || (titleCandidate.length < 2)) continue;
+					List<Pair<String, String>> matched = mSectionIdExtractor.extract(titleCandidate[0]);
+					if ((matched == null) || matched.isEmpty()) continue;
 
 					Utils.println(line);
+					sections.add(line);
 				}
 			}
 		} catch (IOException e) {
 			Utils.printError("Error when trying to extract the document sections!");
 		}
+
+		return sections;
 	}
 }
