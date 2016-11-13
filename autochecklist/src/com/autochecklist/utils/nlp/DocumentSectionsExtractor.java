3ff32f7d7a7fa71a8356c2c4601ea47100e74708
Anderson Rossanez <ra124136@students.ic.unicamp.br>
2016-11-13 13:37:53 -0200

    Improving the sections extraction once again.

--- autochecklist/src/com/autochecklist/utils/nlp/DocumentSectionsExtractor.java
+++ autochecklist/src/com/autochecklist/utils/nlp/DocumentSectionsExtractor.java
@@ -3,9 +3,10 @@ package com.autochecklist.utils.nlp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import com.autochecklist.utils.Pair;
 import com.autochecklist.utils.Utils;
@@ -23,7 +24,10 @@ public class DocumentSectionsExtractor {
 	}
 	
 	public void extract() {
-		Set<String> sections = new HashSet<String>();
+		Utils.println("Extracting the document sections...");
+		
+        List<String> sectionIds = new ArrayList<String>(); // To maintain the document order.
+		Map<String, DocumentSection> sections = new HashMap<String, DocumentSection>();
 
 		BufferedReader bufReader = new BufferedReader(new StringReader(mPlainText));
 		String line = null;
@@ -34,20 +38,42 @@ public class DocumentSectionsExtractor {
 					if ((titleCandidate == null) || (titleCandidate.length < 2)) continue;
 					List<Pair<String, String>> matched = mSectionIdExtractor.extract(titleCandidate[0]);
 					if ((matched == null) || matched.isEmpty()) continue;
+					if (Character.isDigit(titleCandidate[1].charAt(0))) continue;
 
 					String sectionId = titleCandidate[0];
 					if ("APPENDIX_ID".equals(matched.get(0).first)) {
 						sectionId += " " + titleCandidate[1];
 					}
-					
-					if (sections.add(sectionId)) {
-					    Utils.println(line);
-					    mOutputBuilder.addDocumentSection(sectionId, line);
+
+					if (sectionIds.contains(sectionId)) {
+						sectionIds.remove(sectionId);
 					}
+					sectionIds.add(sectionId);
+					
+					DocumentSection currSection = new DocumentSection(sectionId, 
+							line.substring(sectionId.length()));
+					sections.put(sectionId, currSection);
 				}
 			}
 		} catch (IOException e) {
 			Utils.printError("Error when trying to extract the document sections!");
+		}
+
+		for (String sectionId : sectionIds) {
+			String id = sections.get(sectionId).id;
+			String description = sections.get(sectionId).description;
+			Utils.println(id + " " + description);
+		    mOutputBuilder.addDocumentSection(id, description);
+		}
+	}
+	
+	private class DocumentSection {
+		public String id;
+		public String description;
+		
+		public DocumentSection(String id, String description) {
+			this.id = id;
+			this.description = description;
 		}
 	}
 }
