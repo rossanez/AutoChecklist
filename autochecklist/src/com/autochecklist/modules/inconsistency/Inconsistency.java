6f199934a8e3b34f343305458f24bf2e7dfba3f1
Anderson Rossanez <ra124136@students.ic.unicamp.br>
2016-11-18 09:21:24 -0200

    Adding a watch-dog reference detector in the Inconsistency module.

--- autochecklist/src/com/autochecklist/modules/inconsistency/Inconsistency.java
+++ autochecklist/src/com/autochecklist/modules/inconsistency/Inconsistency.java
@@ -1,20 +1,36 @@
 package com.autochecklist.modules.inconsistency;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import com.autochecklist.base.questions.Question;
 import com.autochecklist.base.questions.QuestionCategory;
 import com.autochecklist.base.requirements.Requirement;
 import com.autochecklist.modules.AnalysisModule;
+import com.autochecklist.utils.Pair;
 import com.autochecklist.utils.Utils;
+import com.autochecklist.utils.nlp.ExpressionExtractor;
 
 public class Inconsistency extends AnalysisModule {
 
+	private ExpressionExtractor mExpressionExtractor;
+	private Set<String> mContainsWatchDogReferences;
+
 	public Inconsistency(QuestionCategory questions) {
 		super(questions);
+
+		mExpressionExtractor = new ExpressionExtractor("RegexRules/inconsistency.rules");
+		mContainsWatchDogReferences = new HashSet<String>();
 	}
 
 	@Override
 	public void preProcessRequirement(Requirement requirement) {
 		Utils.println("Inconsistency: Requirement " + requirement.getId());
+
+		if (findWatchDogReferences(requirement.getText())) {
+			mContainsWatchDogReferences.add(requirement.getId());
+		}
 	}
 
 	@Override
@@ -27,5 +43,17 @@ public class Inconsistency extends AnalysisModule {
 	protected void performQuestionAction(Requirement requirement, Question question,
 			int actionType, String actionSubType) {
 		// TODO implement it!
+	}
+
+	private boolean findWatchDogReferences(String text) {
+		if (Utils.isTextEmpty(text)) return false;
+
+		List<Pair<String, String>> matched = mExpressionExtractor.extract(text);
+		if ((matched != null) && (!matched.isEmpty())) {
+			// If it contains results, it is enough.
+			return true;
+		}
+
+		return false;
 	}
 }
